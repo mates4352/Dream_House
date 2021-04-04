2e365af25f16b176ed409f93b75a733e79079a20
Sergey <cabyrkih@mail.ru>
2021-04-04 18:47:21 +0300

    first commit

---
+++ dist/js/app.js
@@ -0,0 +1,6 @@
+const burger = document.querySelector('.burger')
+const menu = document.querySelector('.header__menu')
+burger.addEventListener('click', () => {
+	burger.classList.toggle('active')
+	menu.classList.toggle('active')
+})
