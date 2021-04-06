b704e21ae85cff6b04f36fe62fddd5b26d589430
Sergey <cabyrkih@mail.ru>
2021-04-06 18:56:08 +0300

    section Archmove

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@
 const { src, dest, } = require("gulp")                        //                                                                                                    Документация => https://gulpjs.com/docs/en/api/src , https://gulpjs.com/docs/en/api/dest
 const gulp = require("gulp")                         // Cборщик проектов позволяет собирать проект из разных плагинов                                      Документация => https://gulpjs.com документация на русском https://webdesign-master.ru/blog/docs/gulp-documentation.html
 const fs = require('fs');
+
 // html 
 const htmlmin = require('gulp-htmlmin')
 // const pug           = require("gulp-pug")                     // Шаблонизатор Pug                                                                                   Документация => https://gist.github.com/neretin-trike/53aff5afb76153f050c958b82abd9228
@@ -119,7 +120,7 @@ function css() {
 		.pipe(webpcss())
 		.pipe(media())
 		.pipe(cleanСss({
-			format : 'beautify',
+			format: 'beautify',
 			level: { specialComments: true },
 		}))
 		.pipe(dest(path.build.css))
@@ -178,12 +179,17 @@ function images() {
 		.pipe(dest(path.build.images))
 		.pipe(src(path.src.images))
 		.pipe(newer(path.build.images))
-		.pipe(imagemin({
-			progressive: true,
-			svgoPlugins: [{ removeViewBox: false }],
-			interlaced: true,
-			optimizationLevel: 5  // max 7
-		}))
+		.pipe(imagemin([
+			imagemin.gifsicle({ interlaced: true }),
+			imagemin.mozjpeg({ quality: 75, progressive: true }),
+			imagemin.optipng({ optimizationLevel: 5 }),
+			imagemin.svgo({
+				plugins: [
+					{ removeViewBox: true },
+					{ cleanupIDs: false }
+				]
+			})
+		]))
 		.pipe(dest(path.build.images))
 		.pipe(browsersync.stream())
 }
@@ -234,8 +240,8 @@ function watchFiles() {
 }
 
 //Создаем переменную в которую будем включать все интсрукции которые мы сделали выше gulp.series - позволяет запустить таски по порядку gulp.parallel паралейно другим таскам // Документация => https://gulpjs.com/docs/en/api/concepts
-const build = gulp.series(clean,gulp.parallel(html, css, js, images, fonts));
-const watch = gulp.series(build,gulp.parallel(browserSync,watchFiles,fontsStyle));
+const build = gulp.series(clean, gulp.parallel(html, css, js, images, fonts));
+const watch = gulp.series(build, gulp.parallel(browserSync, watchFiles, fontsStyle));
 
 //----Экспорт таск----//
 
--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,7 @@
 //= components/_burger.js
 //= components/_card3d.js
 
+//= ../library/_fslightbox.js
+
+
+
