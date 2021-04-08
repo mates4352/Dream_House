2c7ea30bb1350b394fdefc2ae0736489b9300b47
Sergey <cabyrkih@mail.ru>
2021-04-08 11:42:54 +0300

    magnific.scss

--- js/app.js
+++ js/app.js
@@ -4,3 +4,13 @@ burger.addEventListener('click', () => {
 	burger.classList.toggle('active')
 	menu.classList.toggle('active')
 })
+
+$('.room__wrapper').magnificPopup({
+	delegate: 'a', 
+	type: 'image',
+	gallery: {
+		enabled: true,
+	},
+	removalDelay: 300,
+	mainClass: 'mfp-fade'
+ });
