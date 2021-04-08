2d19005aab45c678eb6dde790ccfcf31c40a3c57
Sergey <cabyrkih@mail.ru>
2021-04-08 11:42:54 +0300

    magnific.scss

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,11 @@
 //= components/_burger.js
 
-
-
+$('.room__wrapper').magnificPopup({
+	delegate: 'a', 
+	type: 'image',
+	gallery: {
+		enabled: true,
+	},
+	removalDelay: 300,
+	mainClass: 'mfp-fade'
+ });
